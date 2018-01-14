9d8e833544752b993d157ca0841ed326744f9659
Unknown <gaz492@gmail.com>
2018-01-14 21:24:30 +0000

    Remove command check

--- src/bot.js
+++ src/bot.js
@@ -17,18 +17,18 @@ client.on('ready', () => {
 
 client.on('message', message => {
     logging.public(message.guild.name, message.channel.name, message.author.username + '#' + message.author.discriminator, message.toString());
-    if(message.toString().startsWith('!')){
-        commandManager.onCommand(message)
-    }
+    // if(message.toString().startsWith('!')){
+    //     commandManager.onCommand(message)
+    // }
 });
 
 client.on('messageUpdate', (oldMessage, newMessage) =>{
     logging.messageUpdated(oldMessage.guild.name, oldMessage.channel.name, oldMessage.author.username + '#' + oldMessage.author.discriminator, oldMessage.toString(), newMessage.toString());
-    if(newMessage.toString().startsWith('!')){
-        commandManager.onCommand(newMessage)
-    }else{
-
-    }
+    // if(newMessage.toString().startsWith('!')){
+    //     commandManager.onCommand(newMessage)
+    // }else{
+    //
+    // }
 });
 
 client.login(config.bot_token);
